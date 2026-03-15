fe11c1a52cc901c0777acf11e2bcf3244ce29714
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2026-03-15 13:43:57 +0800

    feat: Introduce login page, dark mode and foundational UI components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,45 @@
+import { DarkModeToggle } from "@/components/common/dark-mode-toggle";
+import { Button } from "@/components/ui/button";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import { LockPasswordIcon, Mail01FreeIcons, ThreeDViewIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex items-center justify-items-center w-full">
+      <div
+        className="absolute inset-0 -z-1 h-full w-full bg-white dark:bg-black bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#101010_1px,transparent_1px),linear-gradient(to_bottom,#101010_1px,transparent_1px)] bg-[size:36px_36px]"
+      ></div>
+      <main className="w-1/2 mx-32 flex flex-col gap-4 sm:items-start">
+        <div className="mx-auto flex flex-col w-2/3 text-center mb-2">
+          <div className="flex items-center justify-items-center mb-2 mx-auto">
+            <div className="rounded-lg bg-primary p-2 text-white">
+              <HugeiconsIcon icon={ThreeDViewIcon} size={36} strokeWidth={1.5}></HugeiconsIcon>
+            </div>
+            <h1 className="text-2xl font-bold ml-2">Realtime POS</h1>
+          </div>
+          <p className="text-muted-foreground text-xs">ERICK JAGO NEXTJS TYPESCRIPT EZ MONEY</p>
+          {/* <DarkModeToggle></DarkModeToggle> */}
+        </div>
+        <div className="flex flex-col w-2/3 gap-4 mx-auto">
+          <InputGroup>
+            <InputGroupInput placeholder="Input email" />
+            <InputGroupAddon align={"inline-start"}>
+              <HugeiconsIcon icon={Mail01FreeIcons} size={24} strokeWidth={2} className="text-muted-foreground"></HugeiconsIcon>
+            </InputGroupAddon>
+          </InputGroup>        
+          <InputGroup>
+            <InputGroupInput placeholder="Input password" />
+            <InputGroupAddon align={"inline-start"}>
+              <HugeiconsIcon icon={LockPasswordIcon} size={24} strokeWidth={2} className="text-muted-foreground"></HugeiconsIcon>
+            </InputGroupAddon>
+          </InputGroup>
+          <Button variant={"secondary"} className="w-full">Login</Button>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <Image alt="cashier.jpg" className="w-1/2 h-screen object-cover z-10" width={100} height={100} src={"/img/cashier.jpg"} unoptimized>
+      </Image>
     </div>
   );
 }
